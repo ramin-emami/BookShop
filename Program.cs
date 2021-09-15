d3402b188b4dd2d1f989d7534ccad1a0ee9824e9
Ramin-Laptop <ramin.emami@ymail.com>
2021-09-15 16:56:00 +0430

    Revert "BookShop-CRUD-Before-Identity"
    This reverts commit a5d2cbe02230b6c2ac5ebc56034b146249f47f97.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BookShop
 {
--- wwwroot/select2/src/js/banner.end.js
+++
@@ -1,5 +0,0 @@
-
-  return {
-    define: S2.define,
-    require: S2.require
-  };
